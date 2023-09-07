6b7384bfc71d9f90b187dd0e7d34bebf01da2a71
Ruslanqwd1212 <116277908+Ruslanqwd1212@users.noreply.github.com>
2023-09-07 14:49:15 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,16 @@ import static org.example.MyMath.min;
 public class Main {
     public static void main (String[] args) {
         int intMin = min (5, 3);
-        double doubleMin = min (3.14, 2.71);
+        double doubleMin = min ((int) 3.14, (int) 2.71);
 
         System.out.println("Минимальное из 5 и 3: "  + intMin );
         System.out.println("Минимальное из 3.14 и 2.71: "  + doubleMin );
 
 
     }
-    MainUti mainUti = new MainUti();
 
 
 
-    }
+
+}
 
--- src/main/java/org/example/MyMath.java
+++ src/main/java/org/example/MyMath.java
@@ -4,11 +4,23 @@ public class MyMath {
     public static final double E = 2.718281828459045;
     public static final double PI = 3.141592653589793;
 
-    public static <T extends Comparable<T>> T min(T a, T b) {
-        return a.compareTo(b) < 0 ? a : b;
+    public static int min(int firstNum, int secondNum) {
+        if (firstNum > secondNum) {
+            return firstNum;
+        } else return secondNum;
     }
 
 
+    public static double roundOfLength (int radius){
+        return 2*PI*radius;
+    }
+    public static double roundSquare (int diameter) {
+        return 2 * PI * diameter;
+    }
+
 }
+
+
+
 
 
